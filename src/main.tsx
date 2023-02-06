087ed60661f9742e1b6cbcd02603d28f88ea95d2
yangfree <414871205@qq.com>
2023-02-06 16:42:02 +0800

    更改别名和项目目录调整添加css与处理器支持

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.less'
+
+// 添加css测试文件
+import '@css/test_stylus.styl';
+import "@css/test_less.less";
+import "@css/test_sass.scss";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+
+function __resolve(dir: string) {
+  return path.resolve(__dirname, dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 配置项目别名
+  resolve: {
+    alias: {
+      '@': __resolve('src'),
+      '@css': __resolve('src/testcss'),
+    }
+  }
 })
